3d76b3618a5b7afac1588509647c6249ff9b617a
Xinyuan Wang <40495740+ChelesteWang@users.noreply.github.com>
2022-03-29 19:19:45 +0800

    q12

--- q12.js
+++ q12.js
@@ -3,7 +3,9 @@ function Foo(car) {
     this.bar = () => {
       console.log(this.car);
     };
+    // 原型中赋值 this
     Foo.bar = () => {
+      // this 执行时 确定为 函数体内 this
       console.log(this.car);
     };
   };
@@ -11,7 +13,42 @@ function Foo(car) {
 }
 
 const m = new Foo(3);
+
+//
+
 m.bar();
+
+// 不输出
+
 m.bar();
+
+console.log(this.car);
+// 3
+
 Foo(45);
+
+// this 指向 window
+window.bar();
+
 Foo.bar();
+
+// 1.new
+// m.car = 3
+// m.bar = function(){...}
+
+// 2.
+// const定义对象时,里面的属性还是可以改变的
+// 箭头函数this指向绑定 m 对象
+// m.bar = ()=>{....}
+// foo.bar = () =>{....}
+
+// 3.
+// m.bar 打印3
+
+// 4.
+// Foo方法在全局环境下执行,this指向window
+// window.bar = func(){...}
+// window.car = 45
+
+// 5.
+// foo.bar 打印3
--- q4.js
+++ q4.js
@@ -25,3 +25,32 @@ getName();
 new Foo.getName();
 new Foo().getName();
 new new Foo().getName();
+
+// function getName() {
+//   alert(5);
+// }
+// var getName
+// function Foo() {
+//   getName = function () {
+//     alert(1);
+//   };
+//   return this;
+// }
+// Foo.getName = function () {
+//   alert(2);
+// };
+// Foo.prototype.getName = function () {
+//   alert(3);
+// };
+// var getName = function () {
+//   alert(4);
+// };
+
+// //请写出以下输出结果：
+// Foo.getName();
+// getName();
+// Foo().getName();
+// getName();
+// new Foo.getName();
+// new Foo().getName();
+// new new Foo().getName();
